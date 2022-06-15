7ec74e6400864f825ed2b08e06fec304dc3fcdea
Anna Gubar <68061285+AnnaGubar@users.noreply.github.com>
2022-06-15 20:46:49 +0300

    Updates

---
+++ static/js/Cast.00445ed5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[709],{7623:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var r=n(184);var i=function(){return(0,r.jsx)("div",{children:"Cast"})}}}]);
+//# sourceMappingURL=Cast.00445ed5.chunk.js.map
---
+++ static/js/Reviews.1dfc061e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[753],{7943:function(e,t,i){i.r(t),i.d(t,{default:function(){return r}});var n=i(184);var r=function(){return(0,n.jsx)("div",{children:"Reviews"})}}}]);
+//# sourceMappingURL=Reviews.1dfc061e.chunk.js.map
